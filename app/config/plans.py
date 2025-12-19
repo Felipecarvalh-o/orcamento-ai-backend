65746e8a0e5d3b1af9a64bed561122db726a25da
Felipecarvalh-o <negocioscarvalho@outlook.com>
2025-12-19 12:29:57 -0300

    Create plans.py

---
+++ app/config/plans.py
@@ -0,0 +1,16 @@
+# app/config/plans.py
+
+PLANS = {
+    "OrcaCerto-basic": {
+        "plan": "basic",
+        "monthly_limit": 20
+    },
+    "OrcaCerto-pro": {
+        "plan": "pro",
+        "monthly_limit": 100
+    },
+    "OrcaCerto-premium": {
+        "plan": "premium",
+        "monthly_limit": 9999
+    }
+}
