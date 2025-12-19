3cfc6e2ba510d2cb524cff60a17e8c9083e97032
Felipecarvalh-o <negocioscarvalho@outlook.com>
2025-12-19 09:32:21 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
+from fastapi import FastAPI
+from app.routes import health, auth
+
+app = FastAPI(title="Orçamento AI Backend")
+
+app.include_router(health.router)
+app.include_router(auth.router)
 
