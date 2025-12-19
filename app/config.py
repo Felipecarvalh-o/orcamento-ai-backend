078a93c7ae49ad42221798a58c91b5afeee6dd76
Felipecarvalh-o <negocioscarvalho@outlook.com>
2025-12-19 09:29:13 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
