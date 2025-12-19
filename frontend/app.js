71c2576895853601cce4b2e1756d4d11ed1cf135
Felipecarvalh-o <negocioscarvalho@outlook.com>
2025-12-19 17:13:35 -0300

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,14 @@
+<script>
+const API_URL = "https://SEU_BACKEND.onrender.com";
+
+function getUserId() {
+  return localStorage.getItem("user_id");
+}
+
+function requireAuth() {
+  if (!getUserId()) {
+    window.location.href = "index.html";
+  }
+}
+</script>
 
